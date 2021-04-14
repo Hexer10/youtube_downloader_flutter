be053f4db89d63c0953e4be94b17c4492d8546d0
Mattia <hexer504@gmail.com>
2021-04-14 10:51:52 +0200

    Update windows deploy2

--- tool/compress.ps1
+++ tool/compress.ps1
@@ -2,9 +2,9 @@ $loc = Get-Location
 Set-Location 'build/windows/runner/Release'
 $compress = @{
     LiteralPath = "../../../../LICENSE", "data/", "flutter_windows.dll", "url_launcher_windows_plugin.dll", "window_size_plugin.dll", "youtube_downloader_flutter.exe"
-    DestinationPath = "Flutter Downloader.zip"
+    DestinationPath = "Youtube Downloader.zip"
 
 }
 Compress-Archive -Force @compress
-Move-Item -Path './Flutter Downloader.zip' -Destination '../../../../Flutter Downloader.zip'
+Move-Item -Path './Youtube Downloader.zip' -Destination '../../../../Youtube Downloader.zip'
 Set-Location $loc
